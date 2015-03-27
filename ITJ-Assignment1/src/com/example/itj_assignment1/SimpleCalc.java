8a320935c10cdcee1302defea7761249053190d6
rickDevelopment <205796@sys-chala.nyhq.nytint.com>
2015-03-27 19:16:52 -0400

    Revert fb5a4bb..f23371c
    This rolls back to commit fb5a4bbc3ed8d10dc2fa840c939ba2ca6cf7c254.

--- ITJ-Assignment1/src/com/example/itj_assignment1/SimpleCalc.java
+++ ITJ-Assignment1/src/com/example/itj_assignment1/SimpleCalc.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 public class SimpleCalc extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
